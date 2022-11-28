b7146c3da61299e3bcc81203961de64f27602d43
Horeb Parraud <44978959+HorebParraud@users.noreply.github.com>
2022-11-28 14:21:10 +0100

    🚧 dashbord pages : entities, filters

---
+++ src/components/cards/entityCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <a
+    class="block lg:w-80 lg:h-29 w-rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+    href="/accountant">
+    <div class="flex justify-between align center">
+      <h2 class="m-2 ml-1 font-bold">{{ entity.name }}</h2>
+
+      <p class="m-2 mr-1 text-xs font-medium italic">{{ entity.room_name }}</p>
+    </div>
+
+    <p class="m-2 mr-1 text-xs font-light italic">{{ entity.type }}</p>
+    <div class="flex justify-between gap-5">
+      <span
+        class="inline-block rounded-lg bg-gray-50 p-3"
+        v-bind:class="iconsColor[entity.status]">
+        <svg
+          viewBox="0 0 24 24"
+          class="w-6 h-6">
+          <path
+            fill="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            v-bind:d="icons[entity.type]"/>
+        </svg>
+      </span>
+      <span class="inline-block rounded-lg p-3 w-full bg-gray-50 text-right font-medium ">{{ entity.value }}</span>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "entityCard",
+  props: { entity: Object },
+
+  data() {
+    return {
+      iconsColor: {
+        "on": "text-green-400",
+        "off": "text-gray-400",
+        "unavailable": "text-red-400"
+      },
+      icons: {
+        "sensor": "M5.636 4.575a.75.75 0 010 1.06 9 9 0 000 12.729.75.75 0 01-1.06 1.06c-4.101-4.1-4.101-10.748 0-14.849a.75.75 0 011.06 0zm12.728 0a.75.75 0 011.06 0c4.101 4.1 4.101 10.749 0 14.85a.75.75 0 11-1.06-1.061 9 9 0 000-12.728.75.75 0 010-1.06zM7.757 6.696a.75.75 0 010 1.061 6 6 0 000 8.485.75.75 0 01-1.06 1.061 7.5 7.5 0 010-10.607.75.75 0 011.06 0zm8.486 0a.75.75 0 011.06 0 7.5 7.5 0 010 10.607.75.75 0 01-1.06-1.06 6 6 0 000-8.486.75.75 0 010-1.06zM9.879 8.818a.75.75 0 010 1.06 3 3 0 000 4.243.75.75 0 11-1.061 1.06 4.5 4.5 0 010-6.363.75.75 0 011.06 0zm4.242 0a.75.75 0 011.061 0 4.5 4.5 0 010 6.364.75.75 0 01-1.06-1.06 3 3 0 000-4.244.75.75 0 010-1.06zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z",
+        "light": "M10 1a6 6 0 00-3.815 10.631C7.237 12.5 8 13.443 8 14.456v.644a.75.75 0 00.572.729 6.016 6.016 0 002.856 0A.75.75 0 0012 15.1v-.644c0-1.013.762-1.957 1.815-2.825A6 6 0 0010 1zM8.863 17.414a.75.75 0 00-.226 1.483 9.066 9.066 0 002.726 0 .75.75 0 00-.226-1.483 7.553 7.553 0 01-2.274 0z",
+        "switch": "M6 12a.75.75 0 01-.75-.75v-7.5a.75.75 0 111.5 0v7.5A.75.75 0 016 12zM18 12a.75.75 0 01-.75-.75v-7.5a.75.75 0 011.5 0v7.5A.75.75 0 0118 12zM6.75 20.25v-1.5a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0zM18.75 18.75v1.5a.75.75 0 01-1.5 0v-1.5a.75.75 0 011.5 0zM12.75 5.25v-1.5a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0zM12 21a.75.75 0 01-.75-.75v-7.5a.75.75 0 011.5 0v7.5A.75.75 0 0112 21zM3.75 15a2.25 2.25 0 104.5 0 2.25 2.25 0 00-4.5 0zM12 11.25a2.25 2.25 0 110-4.5 2.25 2.25 0 010 4.5zM15.75 15a2.25 2.25 0 104.5 0 2.25 2.25 0 00-4.5 0z",
+        "multimedia": "M1 3.5A1.5 1.5 0 012.5 2h15A1.5 1.5 0 0119 3.5v10a1.5 1.5 0 01-1.5 1.5H12v1.5h3.25a.75.75 0 010 1.5H4.75a.75.75 0 010-1.5H8V15H2.5A1.5 1.5 0 011 13.5v-10zm16.5 0h-15v10h15v-10z",
+        "air_conditioner": "M4.5 9.75a6 6 0 0111.573-2.226 3.75 3.75 0 014.133 4.303A4.5 4.5 0 0118 20.25H6.75a5.25 5.25 0 01-2.23-10.004 6.072 6.072 0 01-.02-.496z"
+      },
+    }
+  },
+  methods: {},
+  computed: {
+    computedEntity() {
+      return this.entity
+    }
+  },
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/filter.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="lg:mx-auto w-full lg:mx-0 lg:text-left">
+    <div class="lg:sticky lg:top-4">
+      <details
+        open
+        class="overflow-hidden rounded border border-gray-200">
+        <summary class="flex items-center justify-between bg-gray-100 px-5 py-3 lg:hidden">
+          <svg
+            class="h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"/>
+          </svg>
+        </summary>
+
+        <form class="border-t border-gray-200 lg:border-t-0">
+          <div
+            v-for="filter in filters"
+            :key="filter.id">
+            <fieldset>
+              <legend class="block w-full bg-gray-50 px-5 py-3 text-xs font-medium">
+                {{ filter.name }}
+              </legend>
+
+              <div class="space-y-2 px-5 py-6">
+                <div
+                  class="flex items-center"
+                  v-for="choice in filter.choices"
+                  :key="choice.id">
+                  <input
+                    id="{{ choice.name }}"
+                    type="checkbox"
+                    name="age[type.name]"
+                    class="rounded text-indigo-400"
+                    checked/>
+
+                  <label
+                    for="{{ choice.name }}"
+                    class="ml-3 text-sm font-normal">
+                    {{ choice.name }}
+                  </label>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+
+          <div class="flex justify-between border-t border-gray-200 px-5 py-3">
+            <button
+              name="reset"
+              type="button"
+              class="rounded text-xs font-medium text-gray-600 underline">
+              Reset All
+            </button>
+
+            <button
+              name="commit"
+              type="button"
+              class="rounded bg-indigo-400 px-5 py-3 text-xs font-medium text-white">
+              Apply Filters
+            </button>
+          </div>
+        </form>
+      </details>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "filter",
+  props: { filters: [] },
+  methods: {},
+  computed: {
+    computedFilters() {
+      return this.filters
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
