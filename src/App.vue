859351512f7e73436dde196e200d17b7ac1ac022
Ben <ben.richebois@gmail.com>
2023-01-11 11:38:47 +0700

    CSS: prepare the new Layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-stone-100 p-5 min-h-screen flex flex-col">
+  <div class="bg-stone-100 p-5 min-h-screen h-full overflow-hidden flex flex-col">
     <router-view class="flex-1" />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col items-center gap-5 h-full">
+  <div class="flex flex-col items-center gap-5 h-full overflow-hidden">
     <Header />
-    <div class="bg-white rounded-md shadow-lg px-5 py-10 w-full flex-1">
+    <div class="bg-white rounded-md shadow-lg px-5 py-10 w-full flex-1 overflow-hidden">
       <router-view />
     </div>
   </div>
